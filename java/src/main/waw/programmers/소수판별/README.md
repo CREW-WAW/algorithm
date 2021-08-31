dd87fe2baaca1aac3d9545ecddd2017885f31e54
KimDongki <kdkdongki1997@gmail.com>
2021-08-31 11:51:27 +0900

    [goorm] 소수판별 문제

---
+++ java/src/main/waw/programmers/소수판별/README.md
@@ -0,0 +1,16 @@
+
+# [Goorm] [소수판별](https://level.goorm.io/exam/43238/%EC%86%8C%EC%88%98-%ED%8C%90%EB%B3%84/quiz/1)
+   JAVA
+
+Dong-ki, Kim (Jayden)
+
+---
+
+### 문제 설명
+입력된 수가 소수인지를 판별하는 프로그램을 작성하십시오.
+
+---
+
+### 제한 조건
+- 소수일 경우 'True', 아닐 경우 'False'
+
---
+++ java/src/main/waw/programmers/소수판별/Solution.java
@@ -0,0 +1,20 @@
+package main.waw.programmers.소수판별;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    int input = 7;
+    boolean result = solution(input);
+    System.out.print(result);
+  }
+
+  public static boolean solution(int input) {
+    boolean result = true;
+    for (int i = 2; i < input-1; i++) {
+      if (input % i == 0) 
+        result = false;
+    }
+
+    return result;
+  }
+}
